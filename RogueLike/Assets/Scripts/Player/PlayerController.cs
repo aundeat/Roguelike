afc9a9e03332ebaf1a3975905d2ba5c3caaa83e7
aundet <klymenko144@gmail.com>
2023-11-11 15:13:26 +0100

    task bord animator

--- RogueLike/Assets/Scripts/Player/PlayerController.cs
+++ RogueLike/Assets/Scripts/Player/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        OptimizeCamera();
+       OptimizeCamera();
         PlayerMove();
     }
 
---
+++ RogueLike/Assets/Scripts/levl_controller/bord_animator.cs
@@ -0,0 +1,89 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bord_animator : MonoBehaviour
+{
+    
+    [SerializeField]   private CinemachineVirtualCamera BordCamera;
+
+
+    private GameObject player;
+    private CinemachineVirtualCamera CharacterCamera;
+    private void OnTriggerEnter(Collider other)
+    {
+        player = other.gameObject;
+
+        CharacterCamera = other.GetComponentInChildren<CinemachineVirtualCamera>();
+
+        ChangeCamera();
+
+        StartCoroutine(WaitForButton());
+        
+        StopMovement();
+    }
+
+
+
+
+
+
+    IEnumerator WaitForButton()
+    {
+
+      yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E));
+        {
+            Leave();
+            StartMovement();
+        }
+    
+    }
+
+    private void Leave()
+    {
+        ChangeCamera();
+
+
+    }
+
+    private void StopMovement()
+    {
+       // player.GetComponent<PlayerController>().PlayerMove();
+
+        player.gameObject.SetActive(false);
+
+    }
+    private void StartMovement()
+    {
+        player.gameObject.SetActive(true);
+
+        gameObject.GetComponent<Collider>().enabled = false;
+
+        StartCoroutine(delay());
+    }
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(3);
+
+        gameObject.GetComponent<Collider>().enabled = true;
+    }
+    
+
+    private void ChangeCamera()
+    {
+        if (CharacterCamera.Priority == 0) 
+        {
+            CharacterCamera.Priority = 1;
+
+            BordCamera.Priority = 0;
+        }else  { BordCamera.Priority = 1; CharacterCamera.Priority = 0; }
+
+
+
+    }
+
+
+
+
+}
