8842bccf0015276204b317e3de74d01758bac9c9
aundet <klymenko144@gmail.com>
2023-11-12 09:14:14 +0100

    task animation

--- RogueLike/Assets/Scripts/levl_controller/Mission.cs
+++ RogueLike/Assets/Scripts/levl_controller/Mission.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mission : MonoBehaviour
+public class Interfase
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
--- RogueLike/Assets/Scripts/levl_controller/Task_controller.cs
+++ RogueLike/Assets/Scripts/levl_controller/Task_controller.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Task : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Renderer rend;
+    private void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+
+
+
+
+
+    }
+    void OnMouseEnter()
+    {
+        rend.material.color = Color.red;
+    }
+    private void OnMouseExit()
+    {
+        rend.material.color = Color.green;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
+
+
 }
