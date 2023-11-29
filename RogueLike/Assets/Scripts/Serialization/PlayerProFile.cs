2d5efee48327b4e3012322ad12d5aa68b143fd9f
aundet <klymenko144@gmail.com>
2023-11-29 09:18:13 +0100

    save data

---
+++ RogueLike/Assets/Scripts/Serialization/PlayerProFile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerProFile 
+{
+    public int Playerlvl;
+    public float PlayerDamage;
+
+
+    public PlayerProFile proFile;
+}
